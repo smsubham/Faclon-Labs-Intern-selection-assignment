5462209615fc21b78df9b80d32f5220d6eac7351
subham mishra <smsubham342@gmail.com>
2016-07-17 14:41:41 +0530

    Issues Fixed
    1. button appearing all the time whereas it should appear at the end of
    the current list.
    2. on clicking the Load more button the list shouldn't build from the
    top, add elements without bringing the user at the top of the list.

--- ListView/app/src/main/java/listview/com/listview/MainActivity.java
+++ ListView/app/src/main/java/listview/com/listview/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 
@@ -23,34 +24,33 @@ public class MainActivity extends AppCompatActivity {
 
        items=new ArrayList<String>();
         source=new String[sourceSize];
-
+        Button show_more=(Button)findViewById(R.id.button);
         ListView listView=(ListView)findViewById(R.id.listView);
         FrameLayout footerLayout = (FrameLayout) getLayoutInflater().inflate(R.layout.footerview, null);
         listView.addFooterView(footerLayout);
          setSource();
        dynamicallyAddItems();
+        listView.addFooterView(show_more);
     }
     public void dynamicallyAddItems(){
         int i;
         for(i=0;i<20;i++)//no=20
                 items.add(source[i]);
         no=i;
-        ListView listView=(ListView)findViewById(R.id.listView);
-         arrayAdapter = new ArrayAdapter<String>(this,
+
+       ListView listView=(ListView)findViewById(R.id.listView);
+        arrayAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_expandable_list_item_1,items);
         listView.setAdapter(arrayAdapter);
     }
     public void onClick(View view){
         int id=view.getId();
         int i;
-       if(id==R.id.button2){
+       if(id==R.id.button){
           for(i=no;i<size+no&&i<sourceSize;i++)//adds 20 items each time load more is clicked
                items.add(source[i]);
            no=i; //keeps track of how many items are added , so as start from appropriate position next time
-           ListView listView=(ListView)findViewById(R.id.listView);
-           arrayAdapter = new ArrayAdapter<String>(this,
-                   android.R.layout.simple_expandable_list_item_1,items);
-           listView.setAdapter(arrayAdapter);
+           arrayAdapter.notifyDataSetChanged();
        }
     }
     public void setSource(){ //used to store elements to array
