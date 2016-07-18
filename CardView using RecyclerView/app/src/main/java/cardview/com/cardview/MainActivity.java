2e4ce5233e557142b30ff76b4c152fd47ad58d8f
subham mishra <smsubham342@gmail.com>
2016-07-18 19:59:50 +0530

    CardView using RecyclerView
    CardView using RecyclerView

---
+++ CardView using RecyclerView/app/src/main/java/cardview/com/cardview/MainActivity.java
@@ -0,0 +1,32 @@
+package cardview.com.cardview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // setSupportActionBar(toolbar);
+
+        recyclerView =
+                (RecyclerView) findViewById(R.id.recycler_view);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ CardView using RecyclerView/app/src/main/java/cardview/com/cardview/RecyclerAdapter.java
@@ -0,0 +1,72 @@
+package cardview.com.cardview;
+
+/**
+ * Created by HP on 18-07-2016.
+ */
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private String[] titles = {"Chapter One",
+            "Chapter Two",
+            "Chapter Three",
+            "Chapter Four",
+            "Chapter Five",
+            "Chapter Six",
+            "Chapter Seven",
+            "Chapter Eight"};
+
+    private String[] details = {"Item one details",
+            "Item two details", "Item three details",
+            "Item four details", "Item file details",
+            "Item six details", "Item seven details",
+            "Item eight details"};
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView itemTitle;
+        public TextView itemDetail;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            itemTitle = (TextView)itemView.findViewById(R.id.item_title);
+            itemDetail =
+                    (TextView)itemView.findViewById(R.id.item_detail);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    Snackbar.make(v, "Click detected on item " + position,
+                            Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                }
+            });
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.card_layout, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        viewHolder.itemTitle.setText(titles[i]);
+        viewHolder.itemDetail.setText(details[i]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.length;
+    }
+}
